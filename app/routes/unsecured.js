4d2bdb48a948125ecd420b7a69d7eff335781d2b
unknown <christopher.wolf@citrix.com>
2019-03-04 21:04:12 -0500

    refactor auth service and working registration and login

--- app/routes/unsecured.js
+++ app/routes/unsecured.js
@@ -1,34 +1,25 @@
 import express from 'express';
-import admin from 'firebase-admin';
-import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
+import AuthService from '../services/AuthService'
 
 const router = express.Router();
+const authService = new AuthService();
 
-router.get('/ping', (req, res) => {
-    const users = admin.firestore().collection("users");
-    users.doc('wolf').set({first: 'christ', last:'wolf', phone: 123 })
-      .then(() => res.json({good: "ok"}))
-      .catch((err) => res.status(500).json({error: err}))
-});
 //TODO: create a real config secret
 router.put('/register', (req, res) => {
-    console.log(`Registering user: ${req.body.email}`)
-    const hash = bcrypt.hashSync(req.body.password, 10);
-    const users = admin.firestore().collection("users");
-    users.doc(req.body.email).set({ email: req.body.email, hash: hash})
-      .then(resp => {
-        const token = jwt.sign({ email: req.body.email, hash: hash},'config-secret', { expiresIn: '24h' });
-        console.log(`Sending JWT back ${token}`);
-        res.json({data:{ response: resp, token: token } });
+    console.log(`Registering user: ${req.body.email}`);
+    authService.register(req.body.email, req.body.password)
+      .then(hash => {
+        const token = jwt.sign({ email: req.body.email, hash },'config-secret', { expiresIn: '24h' });
+        res.json({data:{ token } });
       }).catch(err => res.status(500).json({error: err}));
 });
 router.post('/login', (req, res) => {
-    const storedHash = '' //getStoredHash
-    if (bcrypt.compareSync(req.body.password, storedHash)) {
-        res.json({token : 'success'})
-       } else {
-        res.status(401).send({message: 'Username or password is incorrect'})
-       }
+    console.log(`Login user: ${req.body.email}`);
+    authService.login(req.body.email, req.body.password)
+      .then(hash => {
+        const token = jwt.sign({ email: req.body.email, hash },'config-secret', { expiresIn: '24h' });
+        res.json({data:{ token } });
+      }).catch(err => res.status(err.status).json({error: err}));
 })
 export default router;
---
+++ app/services/AuthService.js
@@ -0,0 +1,26 @@
+import admin from 'firebase-admin'
+import bcrypt from 'bcrypt'
+
+const AuthService = () => {
+    return {
+        register: (email, password) => new Promise((resolve, reject) => {
+            const hash = bcrypt.hashSync(password, 10);
+            const users = admin.firestore().collection("users");
+            users.doc(email).set({email, hash})
+            .then(() => resolve(hash))
+            .catch(err => reject({status:500, error: err}));
+        }),
+        login: (email, password) => new Promise((resolve, reject) => {
+            const users = admin.firestore().collection("users");
+            users.doc(email).get()
+              .then(doc => {
+                  const hash = doc.get('hash');
+                  if (hash && bcrypt.compareSync(password, hash)) resolve(hash);
+                  reject({status: 401, message: 'Username or password is incorrect'});
+              })
+              .catch(err => reject({status:500, error: err}));
+        })
+    }
+}
+
+export default AuthService

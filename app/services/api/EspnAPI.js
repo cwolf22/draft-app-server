3bb253d1eaf54ad155c98999a1c388cb3a8132c2
unknown <christopher.wolf@citrix.com>
2019-03-07 09:24:05 -0500

    test js for espn importing

--- app/services/api/EspnAPI.js
+++ app/services/api/EspnAPI.js
@@ -1,3 +1,5 @@
+import HTTPService from '../HTTPService'
+
 class EspnAPI {
     constructor() {
         this.baseUri = `http://fantasy.espn.com/apis/v3/games/flb/seasons/${new Date().getFullYear()}/segments/0/leagues/`;
@@ -5,6 +7,46 @@ class EspnAPI {
 
     getRoster(teamId, leagueId) {
         const uri = `${this.baseUri}${leagueId}?forTeamId=${teamId}&view=mRoster`
+        return new Promise((resolve, reject) => {
+            HTTPService.action('GET', uri)
+                .then(response => response.data)
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+        });
+    }
+
+    getLeague(leagueId) {
+        return new Promise((resolve, reject) => {
+            this.getLeagueInfo(leagueId)
+                .then(info => {
+                    let payload = { info }
+                    this.getLeagueRosters(leagueId)
+                        .then(roster => payload.roster = roster);
+                    return payload;
+                })
+                .then(payload =>resolve(payload))
+                .catch(err => reject(err))
+        });
+    }
+
+    getLeagueRosters(leagueId) {
+        const uri = `${this.baseUri}${leagueId}?view=mRoster`
+        return new Promise((resolve, reject) => {
+            HTTPService.action('GET', uri)
+                .then(response => response.data)
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+        });
+    }
+
+    getLeagueInfo(leagueId) {
+        const uri = `${this.baseUri}${leagueId}`
+        return new Promise((resolve, reject) => {
+            HTTPService.action('GET', uri)
+                .then(response => response.data)
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+        });
     }
     
 }

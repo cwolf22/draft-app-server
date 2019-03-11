7795e31b16b9a2715debc00580fb2ebe1567c65a
unknown <christopher.wolf@citrix.com>
2019-03-11 19:37:04 -0400

    reorganize to leverage bin www

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path'
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 3030;
 
 console.log(`Node ENV: ${process.env.NODE_ENV}`)
 const corsOrigin = [/baitingsheep\.com/]
@@ -17,9 +17,27 @@ app.use(cors({
     optionsSuccessStatus: 200
 }))
 
+app.use(express.json());
 app.use(require('morgan')('dev'));
 app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.urlencoded({limit: '100mb', extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bs/v1/drafter', require('./app'))
 
-app.listen(port, () => console.log('We are live on ' + port));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+module.exports = app;

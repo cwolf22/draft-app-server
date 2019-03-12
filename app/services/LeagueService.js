7a5a3121de88ad648baa96d3e06d0bf17043c6ab
unknown <christopher.wolf@citrix.com>
2019-03-12 14:19:28 -0400

    added closing of browser when done

--- app/services/LeagueService.js
+++ app/services/LeagueService.js
@@ -84,36 +84,42 @@ export default class LeagueService {
     espnAuthorize(user, uname, pass) {
         console.log(`[espn] - loginUser ${uname} : get login form...`);
         return new Promise((resolve, reject) => {
+            
             puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']}).then(async browser => {
-                const page = await browser.newPage();
                 try {
-                    await page.goto(LeagueService.espn.login.page, {waitUntil : 'networkidle0'});
-                    const frames = await page.frames();
-                    const myframe = frames.find(f => f.url().indexOf(LeagueService.espn.login.iframeDomain) > 0);
-                    const email = await myframe.$(LeagueService.espn.login.emailSelector);
-                    const password = await myframe.$(LeagueService.espn.login.passwordSelector);
-                    await email.type(uname)
-                    await password.type(pass)
-                    console.log('[espn] - Submitting Form...');
-                    await Promise.all([
-                        myframe.click(LeagueService.espn.login.submitSelector, {waitUntil : 'networkidle0'}),
-                        page.waitForNavigation( {timeout: 30000 }),
-                    ]);
-                } catch (err) {
-                    console.log('[espn] - Login Failure');
-                    reject(err);
-                    return;
-                }
-                try {
-                    const cookies = await page.cookies();
-                    console.log(`[espn] - Login Successful. Returning ${cookies.length} cookies`);
-                    const profile = new EspnProfile(cookies);
-                    if (!profile.isAuthenticated) throw `[espn] - Unable to Authenticate ${uname} through API`
-                    resolve(profile)
-                } catch (err) {
-                    console.log('[espn] - Failure Fetching User Data');
-                    console.log(err);
-                    reject(err);
+                    const page = await browser.newPage();
+                    try {
+                        await page.goto(LeagueService.espn.login.page, {waitUntil : 'networkidle0'});
+                        const frames = await page.frames();
+                        const myframe = frames.find(f => f.url().indexOf(LeagueService.espn.login.iframeDomain) > 0);
+                        const email = await myframe.$(LeagueService.espn.login.emailSelector);
+                        const password = await myframe.$(LeagueService.espn.login.passwordSelector);
+                        await email.type(uname)
+                        await password.type(pass)
+                        console.log('[espn] - Submitting Form...');
+                        await Promise.all([
+                            myframe.click(LeagueService.espn.login.submitSelector, {waitUntil : 'networkidle0'}),
+                            page.waitForNavigation( {timeout: 30000 }),
+                        ]);
+                    } catch (err) {
+                        console.log('[espn] - Login Failure');
+                        reject(err);
+                        return;
+                    }
+                    try {
+                        const cookies = await page.cookies();
+                        console.log(`[espn] - Login Successful. Returning ${cookies.length} cookies`);
+                        const profile = new EspnProfile(cookies);
+                        if (!profile.isAuthenticated) throw `[espn] - Unable to Authenticate ${uname} through API`
+                        resolve(profile)
+                    } catch (err) {
+                        console.log('[espn] - Failure Fetching User Data');
+                        console.log(err);
+                        reject(err);
+                    }
+                } finally {
+                    console.log('close browser')
+                    await browser.close();
                 }
             });
         });
